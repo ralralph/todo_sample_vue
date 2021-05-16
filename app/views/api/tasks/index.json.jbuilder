03fce74e24b59d263f48925fb546ab15f9ad05b1
setsuohorikawa <ralralph@gmail.com>
2021-05-16 20:28:26 +0900

    curlコマンドの確認がうまくいかない

--- app/views/api/tasks/index.json.jbuilder
+++ app/views/api/tasks/index.json.jbuilder
@@ -1,5 +1,5 @@
 json.set! :tasks do
   json.array! @tasks do |task|
-    json.extract! task, :id, :name, :id_done, :crated_at, :updated_at
+    json.extract! task, :id, :name, :is_done, :created_at, :updated_at
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   namespace :api, format: 'json' do
-    resources :tasks, only: %i[index crate update]
+    resources :tasks, only: %i[index create update]
   end
 end
